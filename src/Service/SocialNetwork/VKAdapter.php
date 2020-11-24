6f4547facdb1ff07bb08e7a78c045c704505f769
a.ananyevsky <a.ananyevsky@agima.ru>
2020-11-24 16:11:40 +0300

    implement pattern adapter for send messages via different social network

---
+++ src/Service/SocialNetwork/VKAdapter.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Service\SocialNetwork;
+
+
+class VKAdapter implements ISocialNetwork
+{
+    public function send(string $message): void {
+        // sending a message using the VK API
+        echo "send via vk";
+    }
+}
